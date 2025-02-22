77daceb3165a86ae8e3d64d94acd50bc21a06923
Hafiz Shakeel <hafizshakeel3495@gmail.com>
2025-02-23 01:47:26 +0900

    Update setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='waste_detection',
+    version='0.1.0',
+    author='Hafiz Shakeel Ahmad Awan',
+    author_email='hafizshakeel1997@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+
+)
+
